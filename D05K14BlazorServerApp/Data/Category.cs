6e073ce69b1393bd63c71581edd2b34185f153d4
TuanDat <pdat100111@gmail.com>
2024-07-07 23:21:04 +0700

    upload source code

---
+++ D05K14BlazorServerApp/Data/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace D05K14BlazorServerApp.Data
+{
+    public class Category
+    {
+        public int CatId { get; set; }
+        [Required (ErrorMessage ="Please fill category name")]
+        public string CatName { get; set; }
+        [Required]
+        [Range(0,1)]
+        public int IsDeleted { get; set; }
+    }
+}
---
+++ D05K14BlazorServerApp/Data/DBMySQL.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace D05K14BlazorServerApp.Data
+{
+    public class DBMySQL
+    {
+        private static string HostName = "localhost";
+        private static string UserName = "root";
+        private static string Password = "";
+        private static string Database = "mobile_shop_php";
+        private static int Port = 3306;
+        
+        public static MySqlConnection GetMySqlConnection()
+        {
+            string connStr = String.Format("Server={0};Database={1};User={2};Password={3};Port={4}",HostName, Database,UserName,Password,Port);
+            return new MySqlConnection(connStr);
+        }
+    }
+}
